f4c00a40924511ed14f7ad0dc62882bc1d50d804
Connor Rosenberg <crosenberg8@gatech.edu>
2021-06-21 16:54:10 -0400

    Add procfile and proper server port code

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.get("/remove", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
